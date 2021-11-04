11c034ebb2567f5f918c728968e21707874406b9
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-11-04 15:06:10 +0100

    array setup

---
+++ our-team-base/main.js
@@ -0,0 +1,39 @@
+const teamContainer=document.querySelector(".team-container");
+const cardInfo=[
+    {
+        imgurl:"img/wayne-barnett-founder-ceo.jpg",
+        name:"Wayne",
+        surname:"Barnett",
+        info:"Founder & CEO",
+    },
+    {
+        imgurl:"img/angela-caroll-chief-editor.jpg",
+        name:"Angela",
+        surname:"Caroll",
+        info:"Chief Editor",
+    },
+    {
+        imgurl:"img/walter-gordon-office-manager.jpg",
+        name:"Walter",
+        surname:"Gordon",
+        info:"Office Manager",
+    },
+    {
+        imgurl:"img/angela-lopez-social-media-manager.jpg",
+        name:"Angela",
+        surname:"Lopez",
+        info:"Social Media Manager",
+    },
+    {
+        imgurl:"img/scott-estrada-developer.jpg",
+        name:"Scott",
+        surname:"Estrada",
+        info:"Developer",
+    },
+    {
+        imgurl:"img/barbara-ramos-graphic-designer.jpg",
+        name:"barbara",
+        surname:"Ramos",
+        info:"Graphic Designer",
+    },
+];
